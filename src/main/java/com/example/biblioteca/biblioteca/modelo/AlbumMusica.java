03000cc2ab2fa700324a0a6261e0d1cebc0f39b7
hebertaragao <hebertaragao.dev@gmail.com>
2025-12-05 11:27:55 -0300

    Class created AlbumMusica

---
+++ src/main/java/com/example/biblioteca/biblioteca/modelo/AlbumMusica.java
@@ -0,0 +1,20 @@
+package com.example.biblioteca.biblioteca.modelo;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("ALBUM")
+public class AlbumMusica extends Item {
+	
+	private Integer numeroFaixas;
+	
+	public Integer getNumeroFaixas() {
+		return numeroFaixas;
+	}
+	
+	public void setNumeroFaixas(Integer numeroFaixas) {
+		this.numeroFaixas = numeroFaixas;
+	}
+
+}
