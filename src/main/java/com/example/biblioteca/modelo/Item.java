2e1e17dd6a15996944336cd377873601193f436b
hebertaragao <hebertaragao.dev@gmail.com>
2025-12-05 13:50:27 -0300

    Update

--- src/main/java/com/example/biblioteca/modelo/Item.java
+++ src/main/java/com/example/biblioteca/modelo/Item.java
@@ -7,49 +7,47 @@ import jakarta.validation.constraints.NotBlank;
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "tipo_item")
 public abstract class Item {
-    public abstract class Item {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
-        @NotBlank(message = "Título é obrigatório")
-        private String titulo;
-        @NotBlank(message = "Autor/Artista é obrigatório")
-        private String autor;
-        @NotBlank(message = "Categoria é obrigatória")
-        private String categoria;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Título é obrigatório")
+    private String titulo;
+    @NotBlank(message = "Autor/Artista é obrigatório")
+    private String autor;
+    @NotBlank(message = "Categoria é obrigatória")
+    private String categoria;
 
-        public Long getId() {
-            return id;
-        }
-
-        public void setId(Long id) {
-            this.id = id;
-        }
-
-        public String getTitulo() {
-            return titulo;
-        }
-
-        public void setTitulo(String titulo) {
-            this.titulo = titulo;
-        }
-
-        public String getAutor() {
-            return autor;
-        }
-
-        public void setAutor(String autor) {
-            this.autor = autor;
-        }
-
-        public String getCategoria() {
-            return categoria;
-        }
-
-        public void setCategoria(String categoria) {
-            this.categoria =
-                    categoria;
-        }
+    public Long getId() {
+        return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria =
+                categoria;
+    }
 }
+
